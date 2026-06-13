3e43e23598eb685ba83369bbe32a2eac6616f165
Zakhar Skorokhodov <zaharsk@gmail.com>
2026-06-13 18:14:53 +0000

    docs: Added separator for init command
    Signed-off-by: Zakhar Skorokhodov <zaharsk@gmail.com>

--- .devcontainer/initialize-command.sh
+++ .devcontainer/initialize-command.sh
@@ -5,3 +5,5 @@ set -ueo pipefail
 if [[ ! -f .devcontainer/.env ]]; then
     cp .devcontainer/.env.example .devcontainer/.env
 fi
+
+### === ### === ### === ### === ### === ### === ### === ### === ### === ###
